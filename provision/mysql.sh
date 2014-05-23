dd23f73e1d27fcb06dd94f6d91ac61f1f7bf3ea4
Jan Ivar Beddari <code@beddari.net>
2014-05-23 12:23:01 +0200

    Moving innodb-setting to mysql script

--- provision/mysql.sh
+++ provision/mysql.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-yum install -y mysql-server
+yum install -y mysql-server augeas
 service mysqld start
 chkconfig mysqld on
 dbpw=$(openssl rand -hex 10)
 echo $dbpw > ~/mysql_password
 /usr/bin/mysqladmin -u root password "$dbpw"
+
+# Set innodb as default storage engine for mysql
+augtool set '/files/etc/my.cnf/target[. = "mysqld"]/default-storage-engine' innodb
 
