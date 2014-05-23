b35a378106bd671e7b5d6d15f41bb017b0ab9720
Jan Ivar Beddari <code@beddari.net>
2014-05-23 13:05:01 +0200

    Set innodb table type before starting mysql

--- provision/mysql.sh
+++ provision/mysql.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 yum install -y mysql-server augeas
+# Set innodb as default storage engine for mysql
+augtool set '/files/etc/my.cnf/target[. = "mysqld"]/default-table-type' innodb
 service mysqld start
 chkconfig mysqld on
 dbpw=$(openssl rand -hex 10)
 echo $dbpw > ~/mysql_password
 /usr/bin/mysqladmin -u root password "$dbpw"
-
-# Set innodb as default storage engine for mysql
-augtool set '/files/etc/my.cnf/target[. = "mysqld"]/default-table-type' innodb
 
