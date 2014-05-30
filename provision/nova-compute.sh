f042d668e1677a2817047f260efb8b3386563683
edpto <tor.ladre@it.uib.no>
2014-05-30 14:19:38 +0200

    Added openstack-neutron to install, qpid config

--- provision/nova-compute.sh
+++ provision/nova-compute.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-yum install -y openstack-nova-compute python-cinderclient openstack-utils
+yum install -y openstack-nova-compute python-cinderclient openstack-utils openstack-neutron
+/usr/bin/openstack-config --set /etc/neutron/neutron.conf DEFAULT rpc_backend neutron.openstack.common.rpc.impl_qpid
+/usr/bin/openstack-config --set /etc/neutron/neutron.conf DEFAULT qpid_hostname 172.16.188.11
+
